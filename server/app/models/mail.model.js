011f7bd930ea70b8506fcd563d422346375b65d0
NghiemLe <nghiemlehong98@gmail.com>
2020-11-05 16:17:09 +0700

    btt

--- server/app/models/mail.model.js
+++ server/app/models/mail.model.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 
 const mailSchema = new mongoose.Schema({
-    user : {type : mongoose.Schema.Types.ObjectId, ref : 'User'}
-    
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    title: { type: String, required: true },
+    content: { type: String, required: true }
 
 })
+
+const Mail = mongoose.model('Mail', mailSchema)
+module.exports = { Mail }
