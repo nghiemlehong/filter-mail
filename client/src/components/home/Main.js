a30d5282bc4908a463773da0fae8f7b62055849b
NghiemLe <nghiemlehong98@gmail.com>
2020-12-20 09:33:07 +0700

    better

--- client/src/components/home/Main.js
+++ client/src/components/home/Main.js
@@ -4,7 +4,6 @@ import {Header} from './Header'
 import { makeStyles } from '@material-ui/core/styles'
 import {Content} from './Content'
 import {SearchAppBar} from './AppBar'
-import {Footer} from '../Footer' 
 
 
 const useStyles = makeStyles({
@@ -27,7 +26,6 @@ export function Main(props)
             <Header/>
             <SearchAppBar/>
             <Content/>
-            <Footer/>
         </Card>
     )
 }
--- server/index.js
+++ server/index.js
@@ -32,21 +32,64 @@ app.use('/user', userRouter)
 app.use('/tag', tagRouter)
 app.use('/mail', mailRouter)
 //SVM
-const { SVC } = require('machinelearn/svm')
 const fs = require('fs')
+let rawdataTrain = fs.readFileSync('public/train.json')
+let rawdataTest = fs.readFileSync('public/test.json')
+let dataTrain = JSON.parse(rawdataTrain)
+let dataTest = JSON.parse(rawdataTest)
+
+let labelsTrain = []
+let mailTextTrain = [] 
+let labelsTest = []
+let mailTextTest = []
+
+function removeVietnameseTones(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g,"a"); 
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g,"e"); 
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g,"i"); 
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g,"o"); 
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g,"u"); 
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g,"y"); 
+    str = str.replace(/đ/g,"d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    // Some system encode vietnamese combining accent as individual utf-8 characters
+    // Một vài bộ encode coi các dấu mũ, dấu chữ như một kí tự riêng biệt nên thêm hai dòng này
+    str = str.replace(/\u0300|\u0301|\u0303|\u0309|\u0323/g, ""); // ̀ ́ ̃ ̉ ̣  huyền, sắc, ngã, hỏi, nặng
+    str = str.replace(/\u02C6|\u0306|\u031B/g, ""); // ˆ ̆ ̛  Â, Ê, Ă, Ơ, Ư
+    // Remove extra spaces
+    // Bỏ các khoảng trắng liền nhau
+    str = str.replace(/ + /g," ");
+    str = str.trim();
+    // Remove punctuations
+    // Bỏ dấu câu, kí tự đặc biệt
+    str = str.replace(/!|@|%|\^|\*|\(|\)|\+|\=|\<|\>|\?|\/|,|\.|\:|\;|\'|\"|\&|\#|\[|\]|~|\$|_|`|-|{|}|\||\\/g," ");
+    return str;
+}
+
+dataTrain.map(x => {
+    if (x.Label === 'spam') labelsTrain.push(1)
+    else labelsTrain.push(0)
+    mailTextTrain.push(removeVietnameseTones(x.EmailText))
+})
+dataTest.map(x => {
+    if (x.Label === 'spam') labelsTest.push(1)
+    else labelsTest.push(0)
+    mailTextTest.push(removeVietnameseTones(x.EmailText))
+})
+
+
 const { CountVectorizer } = require('machinelearn/feature_extraction')
 let cv = new CountVectorizer()
-let rawdata = fs.readFileSync('public/spam.json')
-let data = JSON.parse(rawdata)
-let labels = []
-let mailText = []
 
-data.map(x => {
-    if (x.Label === 'spam') labels.push(1)
-    else labels.push(0)
-    mailText.push(x.EmailText)
-})
-const train = cv.fit_transform(mailText)
+let train = cv.fit_transform(mailTextTrain)
+let test = cv.transform(mailTextTest)
+
 const doChinhXac = (a, b) => {
     let dem = 0;
     for (let i = 0; i < a.length; i++) {
@@ -54,22 +97,25 @@ const doChinhXac = (a, b) => {
     }
     return dem / a.length * 100
 }
+const { SVC } = require('machinelearn/svm')
 const trainData = async () => {
-    const svm = new SVC({ cost: 0.8, gamma: 0.02, kernel: 'RBF' })
+    const svm = new SVC({ cost: 0.5 , gamma: 0.1, kernel: 'SIGMOID' })
     const load = await svm.loadASM()
     console.log('Đang train...')
-    load.fit(train, labels)
+    load.fit(train, labelsTrain)
     console.log('Train xong')
-    console.log(`Độ chính xác của dự đoán : ${doChinhXac(load.predict(train), labels)} %`)
+    console.log(`Độ chính xác của dự đoán : ${doChinhXac(load.predict(test), labelsTest)} %`)
     app.post('/svm', (req, res) => {
         const { content } = req.body
-        const text = cv.transform([content])
+        let removeVietnamese = removeVietnameseTones(content)
+        const text = cv.transform([removeVietnamese])
         let tag = 'Normal'
         if (load.predict(text)[0] === 1) { tag = 'Spam' }
         res.send({ success: true, name: tag })
     })
 }
 trainData()
+
 // Start Server
 server.listen(port, () => console.log('Server started !'))
 
